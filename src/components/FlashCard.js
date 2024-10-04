e60214607faca3a26ed7ad81cc36039accda5b12
Dilushika <dil.weera@austal.com>
2024-10-04 16:15:15 +0800

    Add QuizBar Compnents

---
+++ src/components/FlashCard.js
@@ -0,0 +1,13 @@
+import { Component } from "react"
+
+class FlashCard extends Component{
+    constructor() {
+        super();
+    }
+
+    render () {
+        return (
+            <div></div>
+        );
+    }
+}
