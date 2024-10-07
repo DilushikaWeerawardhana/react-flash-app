cd91b53664e766bf27199b52ea29bdf47625456f
Dilushika <dil.weera@austal.com>
2024-10-07 13:35:24 +0800

    Add FlasCard Functionality

--- src/App.js
+++ src/App.js
@@ -1,27 +1,38 @@
 import { Component } from 'react';
 import './App.css';
 import QuizBar from './components/QuizBar';
+import FlashCard from './components/FlashCard';
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      cardStyle : "Random"
+      cardStyle : "Random",
+      ready : false
     };
   }
 
   userChoice = (cardStyle) => {
     this.setState({
-      cardStyle
+      cardStyle,
+      ready : false
+    });
+  }
+
+  nowReady = () => {
+    this.setState({
+      ready : true
     });
   }
 
   render () { 
-    console.log(this.state.cardStyle);
     return (
-      <div className="App">
+      <div className="App align-items-center d-flex">
+        <div className="container">
           <QuizBar userChoice={this.userChoice} />
+          <FlashCard cardStyle={this.state.cardStyle} nowReady={this.nowReady} ready={this.state.ready} />
+        </div>
       </div>
     );
   }
---
+++ src/components/RegularCard.js
@@ -0,0 +1,19 @@
+function RegularCard(props) {
+    console.log(props.questionData);
+    const question = props.questionData;
+    
+    return(
+        <>
+            <div className="card-back">
+                {question.service}
+            </div>
+            <div className="card-front">
+                <div>{question.desc}</div>
+                <div>{question.cat}</div>
+            </div>
+
+        </>
+        )
+}
+
+export default RegularCard;
